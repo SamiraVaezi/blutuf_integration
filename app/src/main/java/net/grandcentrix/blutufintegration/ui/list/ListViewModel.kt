81c6686ca0e630335b0453fb26ac7e20682feac8
Samira Vaezi <samira.vaezi@grandcentrix.net>
2021-03-01 12:11:31 +0100

    applied scan time out

--- app/src/main/java/net/grandcentrix/blutufintegration/ui/list/ListViewModel.kt
+++ app/src/main/java/net/grandcentrix/blutufintegration/ui/list/ListViewModel.kt
@@ -1,7 +1,8 @@
 package net.grandcentrix.blutufintegration.ui.list
 
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.*
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import net.grandcentrix.blutuf.core.Blutuf
@@ -10,13 +11,18 @@ import net.grandcentrix.blutuf.core.api.BlutufEventResult
 import net.grandcentrix.blutufintegration.data.model.DeviceUiState
 import net.grandcentrix.blutufintegration.data.repo.BluetoothRepository
 
+private const val SCAN_TIMEOUT: Long = 5000
+
 class ListViewModel : ViewModel() {
 
     val uiModel = BluetoothRepository.devicesStateFlow.asLiveData()
 
-    val selectedDevice: LiveData<DeviceUiState?> = BluetoothRepository.selectedDeviceStateFlow.asLiveData()
+    val selectedDevice: LiveData<DeviceUiState?> =
+        BluetoothRepository.selectedDeviceStateFlow.asLiveData()
 
     var bleStateFlow = MutableStateFlow(false)
+
+    val scanState = MutableLiveData(false)
 
     init {
         Blutuf.bleManager.registerEventListener(this::onEvent)
@@ -24,6 +30,7 @@ class ListViewModel : ViewModel() {
 
     override fun onCleared() {
         Blutuf.bleManager.unregisterEventListener(this::onEvent)
+        BluetoothRepository.stopScan()
     }
 
     private fun onEvent(event: BlutufEvent, result: BlutufEventResult) {
@@ -41,15 +48,19 @@ class ListViewModel : ViewModel() {
         }
     }
 
-    @ExperimentalCoroutinesApi
     fun startScan() {
         viewModelScope.launch {
+            scanState.value = true
             BluetoothRepository.scan()
+            Handler(Looper.getMainLooper()).postDelayed({
+                stopScan()
+            }, SCAN_TIMEOUT)
         }
     }
 
     private fun stopScan() {
         BluetoothRepository.stopScan()
+        scanState.value = false
     }
 
     fun onConnectClicked(deviceUiState: DeviceUiState) {
