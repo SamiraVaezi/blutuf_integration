10d30ed96b5067a47298f753f7113f77574948ba
Samira Vaezi <samira.vaezi@grandcentrix.net>
2021-03-08 12:14:25 +0100

    added declarations in koin module

--- app/src/main/java/net/grandcentrix/blutufintegration/di/Modules.kt
+++ app/src/main/java/net/grandcentrix/blutufintegration/di/Modules.kt
@@ -1,6 +1,7 @@
 package net.grandcentrix.blutufintegration.di
 
 import net.grandcentrix.blutufintegration.data.repo.BluetoothRepository
+import net.grandcentrix.blutufintegration.domain.*
 import net.grandcentrix.blutufintegration.ui.detail.DetailViewModel
 import net.grandcentrix.blutufintegration.ui.launch.LaunchViewModel
 import net.grandcentrix.blutufintegration.ui.list.ListViewModel
@@ -9,15 +10,40 @@ import org.koin.dsl.module
 
 val applicationModule = module {
 
-    viewModel { LaunchViewModel(bluetoothRepository = get()) }
+    viewModel {
+        LaunchViewModel(checkPreConditionsUseCase = get())
+    }
 
     viewModel {
-        ListViewModel(bluetoothRepository = get())
+        ListViewModel(
+            scanUseCase = get(),
+            stopScanUseCase = get(),
+            connectUseCase = get(),
+            disconnectUseCase = get(),
+            getDeviceUseCase = get()
+        )
     }
 
     viewModel { (id: String) ->
-        DetailViewModel(id, bluetoothRepository = get())
+        DetailViewModel(
+            id,
+            getDeviceUseCase = get(),
+            connectUseCase = get(),
+            disconnectUseCase = get()
+        )
     }
+
+    factory { ScanUseCase(bluetoothRepository = get()) }
+
+    factory { StopScanUseCase(bluetoothRepository = get()) }
+
+    factory { ConnectUseCase(bluetoothRepository = get()) }
+
+    factory { DisconnectUseCase(bluetoothRepository = get()) }
+
+    factory { GetDeviceUseCase(bluetoothRepository = get()) }
+
+    factory { CheckPreConditionsUseCase(bluetoothRepository = get()) }
 
     single {
         BluetoothRepository()
