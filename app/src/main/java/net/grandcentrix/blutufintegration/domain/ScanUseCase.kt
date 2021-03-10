31ef17c47b24ea6a7b8cc08219c5b3f3cba00237
Samira Vaezi <samira.vaezi@grandcentrix.net>
2021-03-10 16:22:50 +0100

    converted shareflow return type to flow in scan uasecase

--- app/src/main/java/net/grandcentrix/blutufintegration/domain/ScanUseCase.kt
+++ app/src/main/java/net/grandcentrix/blutufintegration/domain/ScanUseCase.kt
@@ -1,13 +1,13 @@
 package net.grandcentrix.blutufintegration.domain
 
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.Flow
 import net.grandcentrix.blutufintegration.data.model.DeviceUiState
 import net.grandcentrix.blutufintegration.data.model.ProcessState
 import net.grandcentrix.blutufintegration.data.repo.BluetoothRepository
 
 class ScanUseCase(private val bluetoothRepository: BluetoothRepository) {
 
-    suspend fun execute() : SharedFlow<ProcessState<List<DeviceUiState>>> {
+    suspend fun execute(): Flow<ProcessState<List<DeviceUiState>>> {
         bluetoothRepository.scan()
         return bluetoothRepository.devicesStateFlow
     }
